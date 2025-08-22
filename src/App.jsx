45cb832261c253e77fbef94cb0c45e249a60fded
Heitor Chans <heitorchans3@gmal.com>
2025-08-21 22:26:13 -0300

    atividade 7 slide 12

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Faq from "./components/Faq.jsx";
+import Pesquisa from "./components/Pesquisa.jsx";
 
 function App() {
   return (
     <>
-      <Faq />
+      <Pesquisa />
     </>
   );
 }
