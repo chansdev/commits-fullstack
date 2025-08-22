3aa67df0fee9090398f8914f4732b41b098a0f4a
Heitor Chans <heitorchans3@gmal.com>
2025-08-21 22:45:31 -0300

    atividade final slide 12

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,40 @@ import "./App.css";
 import Pesquisa from "./components/Pesquisa.jsx";
 
 function App() {
+  const [ultilizadorLogado, setUltilizador] = useState(null);
+  const [nome, setNome] = useState("");
+
+  function setarNome(event) {
+    const nome = event.target.value;
+
+    setNome(nome);
+  }
+
+  function login() {
+    setUltilizador(nome);
+  }
+  function logout() {
+    setUltilizador(null);
+  }
+
+  function mensagem() {
+    if (ultilizadorLogado != "" && ultilizadorLogado != null) {
+      return <p>Olá, {nome} Como vai?</p>;
+    } else {
+      return <p>Você está deslogado.</p>;
+    }
+  }
+
   return (
     <>
-      <Pesquisa />
+      <input
+        type="text"
+        placeholder="Insira seu usuario"
+        onChange={setarNome}
+      />
+      {mensagem()}
+      <button onClick={login}>Logar</button>
+      <button onClick={logout}>Logout</button>
     </>
   );
 }
